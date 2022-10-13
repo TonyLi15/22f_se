9fefa1379f47993a50b1c57b907c74bcb166cbd2
Haowen Li <49584078+TonyLi15@users.noreply.github.com>
2022-10-13 10:43:39 +0900

    Update list.c with comments

--- 1_oct7/list.c
+++ 1_oct7/list.c
@@ -3,33 +3,58 @@
 
 //define a structure "list" which contains data element and pointer pointing to next element
 struct list{
-	int data;
-	struct list *next;
+    int data;
+    struct list *next;
 };
 
 //main function
-int main(int argc, char *argv[]) {
-	struct list *head, *new, *temp;
+int main(void) {
+    //*head is for 1st node, *new and *temp are for node afterwards
+    struct list *head, *new, *temp;
 	
-	//initialization of the first node (aka head)
-	head = (struct list *)malloc(sizeof(struct list));
-	int data = 1;
-	head->data = data;
-	head->next = NULL;
+    //Part 1: initialization of the first node (aka head)
+    
+    //allocate memory for the head element
+    head = (struct list *)malloc(sizeof(struct list));
+    
+    //assign data inside the head element to 1
+    int data = 1;
+    head->data = data;
+
+    //set the pointer pointing to next element be NULL (there is no next element yet)
+    head->next = NULL;
 	
-	//print out the element of the first node
-	printf("%d ", head->data);
+    //print out the element of the 1st node (1)
+    printf("%d ", head->data);
 	
-	//creation of 2nd node to 100th node
-	temp = head;
-	for (int i = 2; i <= 100; i++){
-		data++;
-		new = (struct list*)malloc(sizeof(struct list));
-		new->data = data;
-		new->next = NULL;
-		temp->next = new;
-		printf("%d ", new->data);
-		temp = temp->next;
-	}
-	return 0;
+    //Part 2: Creation of 2nd node to 100th node
+
+    //let head element be temporary element for creating nodes afterwards
+    temp = head;
+
+    //create a for loop from 2nd node to 100th node
+    for (int i = 2; i <= 100; i++){
+	 
+	//increment data itself each time
+	data++;
+	
+	//create new element and allocate memory for each of them
+	new = (struct list*)malloc(sizeof(struct list));
+	    
+	//assign data (incremented before) inside the new element to corresponding value
+	new->data = data;
+	
+	//print out the data inside the newly created elements
+	printf("%d ", new->data);
+	
+	//set the pointer pointing to next element be NULL (there is no next element yet, and also there is no next element after the last element)
+	new->next = NULL;
+	
+	//let temporary element point to the previously created "new" element
+	temp->next = new;
+	
+	//let previously created element to be "temporary" element
+	temp = temp->next;
+    }
+    return 0;
 }
